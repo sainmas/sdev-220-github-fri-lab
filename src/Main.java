1d6659fd07049865447d8e91bbc7ff6c1383e507
mason <sainmason012@gmail.com>
2023-06-11 21:43:52 -0700

    Committing driver file.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+/**
+ * This is the entry point to my sample app.
+ *
+ * @author mason
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * starts the application
+     * @param args command-line args (not used)
+     */
     public static void main(String[] args) {
         System.out.println("Hello world, from SDEV 220!");
     }
